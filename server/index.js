42f62c3f3916339503164f9b5e780b26ce57895f
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2022-08-16 10:17:14 -0300

    cambios en el server

--- server/index.js
+++ server/index.js
@@ -1,37 +1,10 @@
-/* import express from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js'
-import userRoutes from './routes/users.js'
-
-const app = express();
-dotenv.config()
-
-
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
-app.use('/posts', postRoutes)
-app.use('/user',userRoutes)
-
-//const CONNECTION_URL = 'mongodb+srv://Ren_Beccari:AbraCadaBra12@cluster0.muqnu.mongodb.net/projectMovies?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>app.listen(PORT, ()=> console.log(`Server Running on Port: ${PORT}`)))
-    .catch((error)=> console.log(error.message)); 
- */
-
-    
-    import express from 'express';
-    import bodyParser from 'body-parser';
-    import mongoose from 'mongoose';
-    import cors from 'cors';
-    import dotenv from 'dotenv';
-    import postRoutes from './routes/posts.js';
-    import userRoutes from "./routes/user.js";
+import postRoutes from './routes/posts.js';
+import userRoutes from "./routes/user.js";
     
     const app = express();
     dotenv.config()
@@ -42,6 +15,10 @@ mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedT
     
     app.use('/posts', postRoutes);
     app.use('/user', userRoutes);
+
+    app.get('/',(req,res)=>{
+        res.send('APP IS RUNNING')
+    })
     
     
     //const CONNECTION_URL = 'mongodb+srv://Ren_Beccari:AbraCadaBra12@cluster0.muqnu.mongodb.net/projectMovies?retryWrites=true&w=majority'
