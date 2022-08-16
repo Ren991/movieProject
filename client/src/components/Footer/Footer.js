278f888ff1210cf82335d5f8529696a60db80588
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2022-08-16 09:52:45 -0300

    cambios 16/8

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -12,27 +12,20 @@ import { Button, Typography } from '@material-ui/core';
   return (
    
     <div className="footer-distributed">
-        
         <Link to="/" className="title">
         <Typography   variant="h6">Renzo Beccari</Typography>
         </Link>
-       
-        
-        <div className="icons">
-          <a href='https://github.com/Ren991' target="_blank">
-          <img src={github} height="50px"/>
-          </a>
-          <a href='https://portfolio-beccari.vercel.app/' target="_blank">
-          <img src={mail} height="50px"/>
-          </a>
-          <a href='https://www.linkedin.com/in/renzo-beccari/' target="_blank">
-          <img src={linkedin} height="50px"/>
-          </a>
-        
-       
-       
-        </div>
-    
+          <div className="icons">
+            <a href='https://github.com/Ren991' target="_blank">
+              <img src={github} height="50px"/>
+            </a>
+            <a href='https://portfolio-beccari.vercel.app/' target="_blank">
+              <img src={mail} height="50px"/>
+            </a>
+            <a href='https://www.linkedin.com/in/renzo-beccari/' target="_blank">
+              <img src={linkedin} height="50px"/>
+            </a>
+          </div>
     </div>
      
   );
