7ade01b8fc8e84203b6e28a22a99252b01005601
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2022-08-19 10:51:54 -0300

    cambios

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid, CircularProgress, Typography,Paper } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 import useStyles from './styles';
+import Swal from 'sweetalert2'
 
 const Posts = ({ setCurrentId }) => {
   const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  if (!posts.length && !isLoading) return 'No posts';
+  if (!posts.length && !isLoading) return 'NO POST FOUNDED'
+  
 
   return (
     isLoading ? <CircularProgress /> : (
